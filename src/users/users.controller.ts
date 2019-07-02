2f6bd65997f4cdc4e46041846c825318d07b9544
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-02 14:28:49 +0200

    step-05-solution

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Delete, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('error')
+  async error() {
+    throw new HttpException(
+      {
+        status: HttpStatus.NOT_IMPLEMENTED,
+        error: 'This is a custom message',
+      },
+      501,
+    );
+  }
 
   @Get(':email')
   findByEmail(@Param('email') email: string) {
