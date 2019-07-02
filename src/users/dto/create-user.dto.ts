e8bd0adfcdc561583a9ede7704ed424c36679d32
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-02 09:34:13 +0200

    step-01-solution

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+  readonly firstname: string;
+  readonly name: string;
+  readonly age: number;
+}
---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateUserDto {
+  readonly id: number;
+  readonly firstname: string;
+  readonly surname: string;
+  readonly age: number;
+}
