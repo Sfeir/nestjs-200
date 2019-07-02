eeefa7b2b5701ea8c880d6d1bc11969c97f2c6e9
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-02 11:38:27 +0200

    step-04-solution

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,7 +1,6 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [],
@@ -9,11 +8,4 @@ import { LoggerMiddleware } from './logger.middleware';
   controllers: [UsersController],
   exports: [UsersService],
 })
-export class UserModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude({ path: 'users', method: RequestMethod.DELETE })
-      .forRoutes(UsersController);
-  }
-}
+export class UserModule {}
