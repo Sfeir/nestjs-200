bb1121e8418036e240ad195a26a97342b04baa6f
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-03 07:48:59 +0200

    step-09-solution

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { logger } from './shared/logger.middleware';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 }
 bootstrap();
