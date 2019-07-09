9a97ce187bd7e66b94ff10505b79ba8fe8697221
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-09 19:39:54 -0400

    Add api global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(logger);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
