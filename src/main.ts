25b1cea70c4914d4521ce420a7bb44c8078b5e1b
Bellenoue <bruno.bellenoue@gmail.com>
2019-07-10 16:12:39 -0400

    Add api global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './shared/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(logger);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
